08fd349ac163e25d9ea3bb747c016989fca9ba0e
hank <taiwanhank0908@gmail.com>
2023-10-02 18:52:04 +0800

    Initial Commit

---
+++ appUIcopy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appUIcopy
+//
+//  Created by User19 on 2023/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appUIcopy/appUIcopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  appUIcopyApp.swift
+//  appUIcopy
+//
+//  Created by User19 on 2023/10/2.
+//
+
+import SwiftUI
+
+@main
+struct appUIcopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
